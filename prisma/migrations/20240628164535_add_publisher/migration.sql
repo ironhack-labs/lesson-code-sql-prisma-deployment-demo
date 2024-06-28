31b0240faf4a9c6e698518e085700cde19c14d39
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2024-06-28 21:35:26 +0200

    fix: modify add-publisher migration (setting a value for existing records)

--- prisma/migrations/20240628164535_add_publisher/migration.sql
+++ prisma/migrations/20240628164535_add_publisher/migration.sql
@@ -4,5 +4,12 @@
   - Added the required column `publisher` to the `Book` table without a default value. This is not possible if the table is not empty.
 
 */
--- AlterTable
-ALTER TABLE "Book" ADD COLUMN     "publisher" TEXT NOT NULL;
+
+-- Add the column with a default value
+ALTER TABLE "Book" ADD COLUMN "publisher" TEXT DEFAULT 'Unknown Publisher';
+
+-- Update existing records so that they're not NULL
+UPDATE "Book" SET "publisher" = 'Unknown Publisher' WHERE "publisher" IS NULL;
+
+-- Alter the column to be NOT NULL
+ALTER TABLE "Book" ALTER COLUMN "publisher" SET NOT NULL;
